ce4de74422c4a743480d60612c278893bc5f4faa
Jonathan2k19 <56574322+Jonathan2k19@users.noreply.github.com>
2021-02-26 17:53:58 +0100

    I think it finally worked...

---
+++ java_Implementation/src/lexer/LexerTest.java
@@ -0,0 +1,14 @@
+package lexer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LexerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
